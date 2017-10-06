28402f211d610b47ed4bdf7fdbc36452aae513dc
Ailen-Aspe <ailengraceaspe@gmail.com>
2017-10-06 10:48:11 -0700

    function signin added

--- ui/app.js
+++ ui/app.js
@@ -1,53 +1,90 @@
-function showevents1(){
-    var data =$('#idnumber').val();
-    alert(data);
-
-}
-
 
 function showevents()
 {
+     var data =$('#idnumber').val();
+    
+   	$.ajax({
+   		url:'http://127.0.0.1:5000/events/'+data,
+   		type: 'GET',
+   		dataType :'json',
 
-	$.ajax({
-		url:'http://127.0.0.1:5000/events/'+data,
-		type:'GET',
-		dataType:'json',
-		success:function(resp)
-		{
-            $('#data').show();
-			$('#eventdetails').html("");
-			if(resp.status== 'ok')
-			{
-				for(i=0; i<resp.count; i++)
+   		success : function(resp){
+   			if(resp.status == 'ok'){
+
+   				$('#eventdetails').html("");
+   				for(i=0; i<resp.count; i++)
 				{
-					studid=resp.entries[i].studid;
-					event=resp.entries[i].event;
-					eventdate=resp.entries[i].eventdate;
-					signin=resp.entries[i].signin;
-					signout =resp.entries[i].signout;
+	   				firstname = resp.entries[i].firstname;
+	   				lastname =resp.entries[i].lastname;
+	   				event =resp.entries[i].event;
+	   				eventdate = resp.entries[i].eventdate;
+	   				signin = resp.entries[i].signin;
+	   				signout = resp.entries[i].signout;
 
 
-					$('#eventdetails').append(rowdata(studid, event, eventdate, signin, signout));
+	   				$('#eventdetails').append(rowattendancedata(event, eventdate, signin, signout));	   
+	   			}
+	   			$('#attendedTB').show();
+	   			$('#studname').html('Hi, '+ firstname +' '+lastname);
+   			}
+   			else{
+   				alert("Status not ok");
+   			}
 
-				}
-			}
-		},
-		error:function(err)
-		{
-			alert("Error in the programmer");
-		}
+   			
+   		},
+   		error :function (err)
+   		{
+   			$('#errorAlert').show();
+   			$('#idnumber').html('');
+	   		$('#attendedTB').hide();
 
-	});
+
+   		}
+
+   	});
 }
 
 
-function rowdata(studid, event, eventdate, signin, signout)
+function rowattendancedata( event, eventdate, signin, signout)
 {
 	return '<tr class="table-success">'+
-			'<td>'+studid+'</td>'+
 			'<td>'+event+'</td>'+
 			'<td>'+eventdate+'</td>'+
 			'<td>'+signin+'</td>'+
 			'<td>'+signout+'</td>'+
 			'</tr>';
+}
+
+
+function signin(){
+	var uname=$('#admin').val();
+	var pwd= $('#password').val();
+
+	$.ajax({
+		url:'http:127.0.0.1:5000/access',
+		dataType: 'json',
+		type:'POST',
+		data:
+		{
+			admin: uname,
+			password:pwd
+		},
+		success: function(resp){
+			$('#sform').hide();
+			if(resp.status == 'Granted'){
+				
+				$('.section').show();
+				$('.main').show();
+			}
+			else{
+				
+				$('#sform').hide();
+			}
+		},
+		error: function(err){
+			alert("Error");
+		}
+
+	})
 }
