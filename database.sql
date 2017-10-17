f6862fa1be31a7ea5c9847bdac3ba3c5deccda43
Ailen-Aspe <ailengraceaspe@gmail.com>
2017-10-17 14:07:50 -0700

    Events and eventlist duplicate error

--- database.sql
+++ database.sql
@@ -14,10 +14,23 @@ create table studentInfo(
 	course text not null
 )
 
+create table eventlist(
+	studid text ,
+	event text,
+	signin text,
+	signout text
+
+)
+
 create table events(
-	studid text references studentInfo(studid),
 	event text primary key,
-	eventdate date,
+	eventdate text
+);
+
+create table eventdata(
+	event text,
+	eventdate text,
+	studid text,
 	signin text,
 	signout text
 
@@ -29,6 +42,125 @@ create table admin(
 	password text
 )
 
+
+
+create or replace function signinevent(parid text, parevent text, pardate text) returns text as
+	$$
+		declare
+			loc_res text;
+			loc_id text;
+
+		begin
+			loc_id := (select studid from eventdata where studid=parid and event =parevent);
+
+			if loc_id isnull then
+				insert into eventdata(event, eventdate, studid, signin, signout) values(parevent, pardate, parid, 'yes', '');
+				loc_res ='Added';
+
+			else
+
+
+	$$
+	language 'plpgsql'
+
+create or replace function newevent(pareventname text, pareventdate text) returns text as
+	$$ 
+		declare
+			loc_id text;
+			loc_res text;
+
+		begin 
+			select into loc_id event from events where event =pareventname;
+
+			if loc_id isnull then
+				insert into events(event, eventdate) values (pareventname, pareventdate);
+
+				loc_res ='OK';
+			else
+				loc_res ='Data exists';
+			end if;
+				return loc_res;
+		end;
+	$$
+	language 'plpgsql';
+
+--select newevent('GAKOP', '2017-10-10')
+
+create or replace function neweventlist(pareventname text, parstudid text) returns text as
+	$$
+		declare
+			loc_res text;
+			loc_id  text;
+
+		begin
+			select into loc_id studid from eventlist, events where studid = parstudid and events.event = pareventname;
+
+			if loc_id isnull then
+				insert into eventlist(studid, event) values (parstudid, pareventname);
+				loc_res ='OK';
+
+			else
+				loc_res ='Data exists';
+
+			end if;
+				return loc_res;
+		end;
+	$$
+	language 'plpgsql';
+
+--select neweventlist('GAKOP', '2013-1364')
+
+--old
+create or replace function signinstud(parID text, parevent text, pardate text) returns text as
+	$$
+		declare
+			loc_res text;
+			loc_id text;
+			loc_data text;
+		begin
+			
+			select into loc_id studid from eventlist where event = parevent and studid=parID;
+
+			if loc_id isnull then
+				insert into eventlist(studid, event, signin) values (parID, parevent, 'yes');
+				loc_res ='Data added';
+			else
+				update eventlist set signin = 'yes' where studid =parID and event = parevent;
+				loc_res ='Data updated';
+
+			end if;
+				return loc_res;
+
+		end;
+	$$
+	language 'plpgsql';
+
+	select signinstud('2013-1364', 'GAKOP', '2017-10-10')
+--old
+create or replace function signoutstud(parID text, parevent text) returns text as
+	$$
+		declare
+			loc_res text;
+			loc_id text;
+			
+		begin
+			
+			loc_id := (select studid from eventlist where studid =parID and event= parevent);
+
+			if loc_id isnull then
+				loc_res ='Data not found';
+			else
+				update eventlist set signout = 'yes' where studid =parID and event = parevent;
+				loc_res ='Data updated';
+
+			end if;
+				return loc_res;
+
+		end;
+	$$
+	language 'plpgsql';
+
+select signinstud('2013-1364','Palakasan', '2017-10-10')
 create or replace function newadmin(paruname text, parpwd text) returns text as
 	$$
 
@@ -106,50 +238,6 @@ create or replace function newstudents (parID text, parFNAME text, parLNAME text
 	language 'plpgsql'
 
 --select newstudents('2013-1364', 'Ailen Grace', 'Aspe', 'BSEC')
-create or replace function newevent(parevent text, pareventdate date, parid text) returns text as
-	$$
-		declare 
-			loc_id text;
-			loc_res text;
-
-		begin
-			select into loc_id event from events where events.event =parevent;
-
-			if loc_id isnull then
-				insert into events (studid, event, eventdate, signin, signout) values (parid, parevent, pareventdate, '', '');
-				loc_res ='OK';
-			else
-				loc_res ='Data exists';
-			end if;
-				return loc_res;
-		end;
-
-	$$
-	language 'plpgsql'
-
-create or replace function newevent(parID text, parEVENT text, parIN text, parOUT text) returns text as
-	$$
-		declare
-			loc_id text;
-			loc_res text;
-
-		begin 
-			loc_id =(select  events.event from events, studentInfo where events.studid=studentInfo.studid and events.event=parEVENT and events.studid=parID);
-
-			if loc_id isnull then
-				insert into events (studid, event, eventdate, signin, signout) values (parID, parEVENT, now(), parIN, parOUT);
-				loc_res ='OK';
-
-			else
-				loc_res='Data exists';
-
-			end if;
-				return loc_res;
-		end;
-	$$
-	language 'plpgsql'
---select newevent('2013-1364', 'COE Week 2017', 'yes', 'no')
-select newevent('2013-1364', 'GAKOP 2017', 'yes', 'no')
 
 
 create or replace function dropEvent(parEvent text) returns text as
@@ -174,19 +262,6 @@ create or replace function dropEvent(parEvent text) returns text as
 
 --select dropEvent('COE Week 2017');
 
-create or replace function getevents(in parID text, out text, out text, out text, out date, out text, out text) returns setof record as
-	$$
-		select  studentInfo.firstname, studentInfo.lastname, events.event, events.eventdate, events.signin, events.signout from events, studentInfo where studentInfo.studid = parID and studentInfo.studid = events.studid;
-	$$
-	language 'sql';
---select getevents('2013-1364')
-
-create or replace function getliststudsinevent(in parEvent text, out text, out text, out text, out text) returns setof record as
-	$$
-		select events.studid, studentInfo.course, events.signin, events.signout from studentInfo, events where events.event = parEvent and events.studid =studentInfo.studid;
-	$$
-	language 'sql';
---select getliststudsinevent('COE Week 2017')
 
 
 create or replace function getstudentdata(in parID text, out text, out text, out text, out text) returns setof record as
@@ -197,10 +272,23 @@ create or replace function getstudentdata(in parID text, out text, out text, out
 	language 'sql'
 --select getstudentdata('2013-1364')
 
-create or replace function getlistevents(out text, out date) returns setof record as
+create or replace function getevents(in parID text, out text, out text, out text, out text, out text, out text) returns setof record as
+	$$
+		select studentInfo.firstname, studentInfo.lastname, events.event, events.eventdate, eventlist.signin, eventlist.signout from studentInfo, eventlist, events where studentInfo.studid=parID and studentInfo.studid=eventlist.studid and eventlist.event=events.event;
+	$$
+	language 'sql'
+
+--select getevents('2013-1364')
+create or replace function getlistevents(out text, out text) returns setof record as
 	$$
 		select event, eventdate from events;
 	$$
 	language 'sql';
 
 --select getlistevents()
+
+create or replace function getliststudinevents(in parevent text, out text, out text, out text, out text) returns setof record as
+	$$
+		select studentInfo.studid, studentInfo.course, eventlist.signin, eventlist.signout from eventlist, studentInfo where eventlist.event=parevent and studentInfo.studid =eventlist.studid;
+	$$
+	language 'sql';
